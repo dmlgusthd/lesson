67a717723600fcd4ac97e93878a5676f6c8fa85d
Administrator <Administrator@smart20115>
2016-12-16 14:06:51 +0900

    member 학습 초기

---
+++ spms/src/main/java/spms/filter/CharacterEncodingFilter.java
@@ -0,0 +1,31 @@
+package spms.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class CharacterEncodingFilter implements Filter{
+	FilterConfig config;
+	
+	
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
+	}
+	
+	
+	public void doFilter(
+			ServletRequest request, ServletResponse response,
+			FilterChain nextFilter) throws IOException, ServletException {
+		System.out.println("CharacterEncodingFilter ½ÇÇà");
+		request.setCharacterEncoding(config.getInitParameter("encoding"));
+		nextFilter.doFilter(request, response);
+	}
+
+	
+	public void destroy() {}
+}
---
+++ spms/src/main/java/spms/servlets/MemberListServlet.java
@@ -0,0 +1,51 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+// ServletContext에 보관된 Connection 객체 사용  
+//UI 출력 코드를 제거하고, UI 생성 및 출력을 JSP에게 위임한다.
+@WebServlet("/member/list")
+public class MemberListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(
+			HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try{
+			ServletContext sc = this.getServletContext();
+			Connection connection = (Connection)sc.getAttribute("conn");
+			
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(connection);
+			
+			List<Member> members = memberDao.selectList();
+			// request에 회원 목록 데이터 보관한다.
+			request.setAttribute("members", members);
+			
+			// JSP로 출력을 위임한다.
+			RequestDispatcher rd = request.getRequestDispatcher(
+					"/member/MemberList.jsp");
+			rd.include(request, response);
+			
+		} catch (Exception e) {
+			throw new ServletException(e);
+			
+		} 
+
+	}
+}
